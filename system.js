86c28b2f23e1bc2132aaf955fb864b21994c1326
CreatingInfinity <83005369+CreatingInfinity@users.noreply.github.com>
2023-02-03 21:36:33 +0800

    Add files via upload

---
+++ system.js
@@ -0,0 +1,12 @@
+function divclick()
+{
+    window.location.href = 'index.html';
+}
+function civclick()
+{
+    window.location.href = 'edu.html';
+}
+function botclick()
+{
+    document.documentElement.scrollTop = 10000;
+}
